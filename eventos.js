3b5780fba7c5137643735eea4122cd12fde24384
Stefi <144570099+stefi1211@users.noreply.github.com>
2024-08-27 23:33:27 +0000

    Agregada la funcionalidad de alerta al div

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var miDiv = document.getElementById('mi-div');
+    miDiv.addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
